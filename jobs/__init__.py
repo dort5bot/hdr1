dfad85acb04bda72efd4e26e586cc434645ee899
dort5bot <dersdep@gmail.com>
2025-09-11 22:16:17 +0300

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,1 +1,7 @@
+# jobs/__init__.py
+# jobs package için basit init dosyası
+
+# Burada genellikle package-level import gerekmez, boş bırakabiliriz
+# Eğer scheduler objesini package-level açmak istersen aşağıdaki gibi ekleyebilirsin:
+# from .scheduled_tasks import scheduler
 
