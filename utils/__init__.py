3be1f58fd2a10acca3d40e14569f93059a7fb0fc
dort5bot <dersdep@gmail.com>
2025-09-11 22:15:03 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,8 @@
+# utils/__init__.py
+# Package-level imports for convenience
+
+# Relative import kullanıyoruz, bu sayede Render ve local çalışmada hata vermez
 from . import email_utils
-from . import excel_processor
 from . import file_utils
+from . import excel_processor
+from . import handler_loader
