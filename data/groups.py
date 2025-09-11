9ff336dae01e2e5e4b0172a9ba5bcdf3127b15f8
dort5bot <dersdep@gmail.com>
2025-09-12 00:32:34 +0300

    Create groups.py

---
+++ data/groups.py
@@ -0,0 +1,86 @@
+[
+  {
+    "no": "GRUP_1",
+    "name": "ANTALYA",
+    "iller": "AFYON,AKSARAY,ANKARA,ANTALYA,BURDUR,ÇANKIRI,ISPARTA,KARAMAN,KAYSERİ,KIRIKKALE,KIRŞEHİR,KONYA,UŞAK",
+    "email": "dersdep@gmail.com"
+  },
+  {
+    "no": "GRUP_2",
+    "name": "MERSİN",
+    "iller": "ADANA,ADIYAMAN,BATMAN,BİNGÖL,BİTLİS,DİYARBAKIR,ELAZIĞ,GAZİANTEP,HAKKÂRİ,HATAY,KAHRAMANMARAŞ,KİLİS,MALATYA,MARDİN,MERSİN,MUŞ,OSMANİYE,SİİRT,ŞANLIURFA,ŞIRNAK",
+    "email": "dersdep@gmail.com"
+  },
+  {
+    "no": "GRUP_3",
+    "name": "İZMİR",
+    "iller": "AFYON,AYDIN,BURDUR,ISPARTA,İZMİR,ÇANAKKALE,MANİSA,MUĞLA,UŞAK",
+    "email": "GRUP_3@gmail.com"
+  },
+  {
+    "no": "GRUP_4",
+    "name": "BURSA",
+    "iller": "BALIKESİR,BURSA,ÇANAKKALE,DÜZCE,KOCAELİ,SAKARYA,TEKİRDAĞ,YALOVA",
+    "email": "GRUP_4@gmail.com"
+  },
+  {
+    "no": "GRUP_5",
+    "name": "BALIKESİR",
+    "iller": "BALIKESİR,ÇANAKKALE",
+    "email": "GRUP_5@gmail.com"
+  },
+  {
+    "no": "GRUP_6",
+    "name": "KARADENİZ",
+    "iller": "ARTVİN,BAYBURT,ÇANKIRI,ERZİNCAN,ERZURUM,GİRESUN,GÜMÜŞHANE,ORDU,RİZE,SAMSUN,SİNOP,SİVAS,TOKAT,TRABZON",
+    "email": "GRUP_6@gmail.com"
+  },
+  {
+    "no": "GRUP_7",
+    "name": "ERZİNCAN",
+    "iller": "BİNGÖL,ERZİNCAN,ERZURUM,GİRESUN,GÜMÜŞHANE,KARS,ORDU,SİVAS,ŞIRNAK,TOKAT,TUNCELİ",
+    "email": "GRUP_7@gmail.com"
+  },
+  {
+    "no": "GRUP_8",
+    "name": "ESKİŞEHİR",
+    "iller": "AFYON,ANKARA,BİLECİK,ESKİŞEHİR,UŞAK",
+    "email": "GRUP_8@gmail.com"
+  },
+  {
+    "no": "GRUP_9",
+    "name": "KÜTAHYA",
+    "iller": "AFYON,ANKARA,BİLECİK,BOZÜYÜK,BURSA,ESKİŞEHİR,KÜTAHYA,UŞAK",
+    "email": "GRUP_9@gmail.com"
+  },
+  {
+    "no": "GRUP_10",
+    "name": "ÇORUM",
+    "iller": "AMASYA,ANKARA,ÇANKIRI,ÇORUM,KASTAMONU,KAYSERİ,KIRIKKALE,KIRŞEHİR,SAMSUN,TOKAT,YOZGAT",
+    "email": "GRUP_10@gmail.com"
+  },
+  {
+    "no": "GRUP_11",
+    "name": "DENİZLİ",
+    "iller": "AFYON,AYDIN,BURDUR,DENİZLİ,ISPARTA,İZMİR,MANİSA,MUĞLA,UŞAK",
+    "email": "GRUP_11@gmail.com"
+  },
+  {
+    "no": "GRUP_12",
+    "name": "AKHİSAR",
+    "iller": "MANİSA",
+    "email": "GRUP_12@gmail.com"
+  },
+  {
+    "no": "GRUP_13",
+    "name": "DÜZCE",
+    "iller": "BOLU,DÜZCE,EDİRNE,İSTANBUL,KARABÜK,KIRKLARELİ,KOCAELİ,SAKARYA,TEKİRDAĞ,YALOVA,ZONGULDAK",
+    "email": "GRUP_13@gmail.com"
+  },
+  {
+    "no": "GRUP_14",
+    "name": "TUNCAY",
+    "iller": "AKSARAY,ANKARA,KAHRAMANMARAŞ,KIRIKKALE,KIRŞEHİR",
+    "email": "GRUP_14@gmail.com"
+  }
+]
